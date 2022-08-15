0300d23dbfdb412aaf428983eaff993ef2be4fca
ToshiHayashi <32119345+ToshiHayashi@users.noreply.github.com>
2022-08-15 16:19:18 +0200

    Update_preprocess.py

--- sliding_window/preprocess.py
+++ sliding_window/preprocess.py
@@ -11,7 +11,7 @@ def readfile(filename):
     return ans
 
 
-d=3000
+d=660
 start=[0,90,150,210,270,360,450,510,570,630,690]
 end=[60,120,180,240,300,420,480,540,600,660,720]
 output=np.zeros([0,d])
@@ -24,4 +24,4 @@ for i in range(20):
             output=np.concatenate([output,data],axis=0)
         except:
             pass
-makefile(output,"normal_breath3000.pkl")
+makefile(output,"normal_breath660.pkl")
